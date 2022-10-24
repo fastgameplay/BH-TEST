7c49dfed110e95a84b7ce04841d72321ce5ec5d0
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2022-10-24 20:11:46 +0400

    Dash Added

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,27 +8,47 @@ public class PlayerMovement : MonoBehaviour{
             _movement = new Vector3(value.x,0f,value.y);
         }  
     }
-
-    [SerializeField] Transform _playerModel;
+    [Header("Movement")]
     [SerializeField] Transform _directionTarget;
     [SerializeField] float _speed;
+    [SerializeField] float _dashDistance;
+    [Header("Rotation")]
+    [SerializeField] Transform _playerModel;
     [SerializeField] float _rotationSpeed;
     Vector3 _movement;
     void Update(){
         Movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        Move();
+        
+        Move(_movement, _speed);
 
-        Aim();
-    }
 
-    private void Move(){
-        transform.Translate(_movement * Time.deltaTime * _speed, _directionTarget);
-    }
-    private void Aim(){
-        if(_movement != Vector3.zero){
-            Vector3 direction = _directionTarget.forward * _movement.x + _directionTarget.right * -_movement.z;
+        Aim(_movement);
 
-            _playerModel.forward = Vector3.Slerp(_playerModel.forward, direction.normalized, Time.deltaTime * _rotationSpeed);
+        if (Input.GetKeyDown("space")){
+            Dash();
         }
+    }
+
+    private void Move(Vector3 movementDirection, float speed){
+        transform.Translate(movementDirection * Time.deltaTime * speed, _directionTarget);
+    }
+    private void Aim(Vector3 movementDirection){
+        if(movementDirection != Vector3.zero){
+            Vector3 aimDirection = _directionTarget.forward * movementDirection.x + _directionTarget.right * -movementDirection.z;
+
+            _playerModel.forward = Vector3.Slerp(_playerModel.forward, aimDirection.normalized, Time.deltaTime * _rotationSpeed);
+        }
+    }
+
+    public void Dash(){
+        if(_movement != Vector3.zero)
+            Move(_movement, _dashDistance);
+        else{
+            Aim(Vector3.forward);
+            Move(Vector3.forward, _dashDistance);
+        }
+
+        
+
     }
 }
