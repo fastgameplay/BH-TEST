69557bcf486b91ef2f7cb8fbc20da44a91d304dd
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2022-10-25 23:08:38 +0400

    DeathController Beta

--- Assets/Scripts/Player/DeathController.cs
+++ Assets/Scripts/Player/DeathController.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DeathController : MonoBehaviour{
-    
-    IEnumerable hit(float Seconds){
+    private Material playerMaterial;
+    void Awake(){
+        playerMaterial = GetComponent<MeshRenderer>().material;
+    }
 
+    IEnumerable hit(float Seconds){
+        Color oldColor = playerMaterial.color;
+        playerMaterial.color = Color.red;
+        yield return new WaitForSeconds(Seconds);
+        playerMaterial.color = oldColor;
     }
 }
